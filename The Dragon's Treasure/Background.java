88251b7483df4c46d5dad7c37dc10f7d3b386e1c
Andrew He <43284720+AndyHuh98@users.noreply.github.com>
2019-05-20 17:46:56 -0500

    Update Background.java

--- The Dragon's Treasure/Background.java
+++ The Dragon's Treasure/Background.java
@@ -1,5 +1,10 @@
 import java.awt.Graphics;
 
+/*
+This class was created to in order to correctly place the "background" texture of the game. The background is added as a sprite to
+the ArrayList of sprites that's iterated through.
+*/
+
 public class Background extends Sprite {
 	public Background() {
 		super("background.png");
