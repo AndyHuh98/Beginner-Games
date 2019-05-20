3769f0cbe042a1b1d88bb0e53bf1936eb02e03c2
Andrew He <43284720+AndyHuh98@users.noreply.github.com>
2019-05-20 18:20:24 -0500

    Update Projectile.java

--- The Dragon's Treasure/Projectile.java
+++ The Dragon's Treasure/Projectile.java
@@ -8,6 +8,11 @@ import javafx.scene.shape.Rectangle;
 import java.io.File;
 import java.io.IOException;
 
+/*
+This is a superclass that grants the Arrow and Fireball classes the "launch" capability - a method which moves the "sprite" a given distance
+in a given direction.
+*/
+
 public class Projectile extends Sprite{
 	public Projectile(String pngName) {
 		super(pngName);
@@ -18,15 +23,17 @@ public class Projectile extends Sprite{
 		g.drawImage(getImage(), getX(), getY(), 50, 50, null);
 	}
 	
+	//launch is used by Arrow and Fireball.
 	public synchronized double launch(int distance, double launchX, double launchY) {
 		double direction = Math.sqrt((launchX * launchX) + (launchY * launchY));
 		
+		//It uses basic application of the pythagorean theorem.
 		double xDistance = (launchX * distance) / direction;
 		double yDistance = (launchY * distance) / direction;
 		
 		int x = (int) (getX() + (int) xDistance);
 		int y = getY() + (int) yDistance;
-		setX(x);
+		setX(x); 
 		setY(y);
 		
 		return distance;
