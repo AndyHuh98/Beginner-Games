d430a14abef405d2c65d685d70a55ea6dfab5cd2
Andrew He <43284720+AndyHuh98@users.noreply.github.com>
2019-05-21 21:21:52 -0500

    Update Controller.java

--- The Dragon's Treasure/Controller.java
+++ The Dragon's Treasure/Controller.java
@@ -12,11 +12,22 @@ import java.util.Timer;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
+/*
+The controller class is created along with Model and View in the Model-View-Controller design pattern,
+which I felt would suit the game best. This controller class essentially contains the "controls"
+for the game, such as character movement, pausing/resuming/loading/saving/etc. Controller also contains the main 
+method, where the game is called and run.
+*/
+
 class Controller implements MouseListener, KeyListener 
 {
+    /*
+    Here, a model and view object are going to be created for each call.
+    */
     Model model;
-    //BuriedTreasureModel BTModel;
     View view;
+    //Instructions are listed as a global variable. This allows for easy modification of instructions above,
+    //so in various calls to pull up the instructions, you don't have to change long strings of text.
     String instructions = 
     		String.format("====INSTRUCTIONS TO PLAY THIS GAME====\n"
     				+ "PRESS R: Start and/or Resume Gameplay\n"
@@ -35,20 +46,26 @@ class Controller implements MouseListener, KeyListener
     				+ "TREASURE HUNTING MODE:\n"
     				+ "You have 10 attempts to find the buried treasure!\n"
     				+ "Press the SPACE BAR to dig.\n");
-
+    /* Constructor for the Controller class. Creates a new model (logic for the game) and view (display)
+    as well as brings up the instruction manual upon opening the JFrame. */
     Controller() throws IOException, Exception {
         model = new Model();
         view = new View(this);
         JOptionPane.showMessageDialog(null, instructions);
     }
     
+    //Getters for the view method to use, basically.
     public Model getModel() { return model; }
     public String getInstructions() { return instructions; }
 
+    //Main method in controller that calls the model's update method, which is used to update the 
+    //location of the sprite and allow for movement animations. Go to model.java and view update method
+    //for more information.
     public synchronized void update(Graphics g) {
         model.update(g);
     }
-
+    
+    /* Mouse click events. These are not used aside from the "right click" which was used for testing purposes.*/
     public void mousePressed(MouseEvent e) {     }
     public void mouseReleased(MouseEvent e) {    }
     public void mouseEntered(MouseEvent e) {    }
@@ -60,9 +77,12 @@ class Controller implements MouseListener, KeyListener
     	}
     }
     
+    /* Key press events - when a user presses a certain key, an action is done in game. */
     public void keyPressed(KeyEvent e) {
+    	/* The first four utilize the arrow keys to move the hero sprite left, right, up, and down. */
     	if (e.getKeyCode() == KeyEvent.VK_LEFT) {
     		model.moveHero("left");
+		//after each movement, manually repaint the view (updating the sprite's location)
     		view.repaint();
     	} 
     	if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
@@ -77,13 +97,17 @@ class Controller implements MouseListener, KeyListener
     		model.moveHero("down");
     		view.repaint();
     	}
-	    if (model.getDragon().getDragonIsNotDead()) {
-	    	if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-	    		model.shootArrow();
-	    		view.repaint();
-	    	}
+	//If the dragon is not dead, the hero can shoot arrows at the dragon to deal damage
+	if (model.getDragon().getDragonIsNotDead()) {
+	    if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+	    	model.shootArrow();
+	    	view.repaint();
+	    }
+	//If the dragon IS dead, and the hero has walked thru the portal that spawns upon its death,
+	//begin the treasure hunting phase of the game
     	} else if (!(model.getDragon().getDragonIsNotDead()) && model.getWalkedThruPortal()) {
     		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			//hero gets x digs (default is 10. can change in Model.java)
     			if (model.getDigsRemaining() > 0) {
     				model.dig();
 	    			int x = model.getHero().getX();
@@ -92,12 +116,17 @@ class Controller implements MouseListener, KeyListener
 	    			int x2 = model.getTreasureCoordinates().get(0);
 	    			int y2 = model.getTreasureCoordinates().get(1);
 	    			
+				/* Two rectangles are created. One using the hidden treasure's location,
+				the other using the hero's location upon digging (sprite boundaries). */
 	    			Rectangle rect1 = new Rectangle(x, y, 75, 100);
 	    			Rectangle rect2 = new Rectangle(x2, y2, 100, 100);
 	    		
 	    			String digMessage = String.format("Hero dug at: %d, %d%n", x, y);
 	    			JOptionPane.showMessageDialog(null, digMessage);
 	    			
+				/* If the hero digs at the right spot (intersects treasure boundaries),
+				   the treasure appears and the hero gains a treasure which can be used
+				   to increase damage (optionally) */
 	    			if (rect1.intersects(rect2)) {
 	    				Treasure treasure = new Treasure();
 	    				treasure.setX(x2);
@@ -105,18 +134,20 @@ class Controller implements MouseListener, KeyListener
 	    				model.TreasureFound();
 	    				model.getSprites().add(treasure);
 	    				model.spendTreasure();
+					/* UNCOMMENT IF GAME DOESNT PROPERLY WORK
 	    				try {
 						model.saveGame();
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
+					*/
 	    				
 	    				int reply = JOptionPane.showConfirmDialog(null, "Would you like to face the next dragon?", "Exit", JOptionPane.YES_NO_OPTION);
 					if (reply == JOptionPane.YES_OPTION) {
 						JOptionPane.showMessageDialog(null, "Good luck, adventurer.");
 					      	try {
 							model.nextDragon();
-							model.saveGame();
+							//model.saveGame();
 							model = new Model();
 					      	} catch (IOException e1) {
 							e1.printStackTrace();
@@ -132,7 +163,7 @@ class Controller implements MouseListener, KeyListener
 			      		JOptionPane.showMessageDialog(null, "Good luck, adventurer.");
                       			try {
                     				model.nextDragon();
-                    				model.saveGame();
+                    				//model.saveGame();
 						model = new Model();
                       			} catch (IOException e1) {
 						e1.printStackTrace();
@@ -146,8 +177,15 @@ class Controller implements MouseListener, KeyListener
     	}	
     }
     
+    /* Key Typed Events -- Pretty much the same as Key Press; just more convenient to use Key Press for
+    things like space, arrow keys which don't necessarily create a character value */
     public void keyReleased(KeyEvent e) { }
     public synchronized void keyTyped(KeyEvent e) { 
+    	// If the dragon is not dead, allow for pausing, saving, loading, resuming, etc.
+	// The things below should be fairly straightforward. Game is paused after most menu actions, such
+	// as bringing up hero health, etc. At some point, a health bar will/should be added.
+	// After exiting the JOptionPanes, game is normally resumed. Thread will need to be restarted,
+	// and the fireball creation/dragon movement along with it.
     	if (model.getDragon().getDragonIsNotDead()) {
 	    	if (e.getKeyChar() == 'r') {
 	    		model.resumeGame();
@@ -214,7 +252,7 @@ class Controller implements MouseListener, KeyListener
 	               model.createNewFireball();
 	               model.moveDragon();
 	               Thread thread = new Thread(new ProjectileLauncher(model, view));
-	       		   thread.start();
+	       	       thread.start();
 	            }
 	    	}
 	    	if (e.getKeyChar() == 'l') {
@@ -224,6 +262,8 @@ class Controller implements MouseListener, KeyListener
 	            		try {
 	            			File file = new File("DragonTreasure.txt");
 					model.loadGame(file);
+					/* when loading, create new model so that the dragon can be updated
+					with the correct dragon level (and other attributes) */
 					model = new Model();
 					view.repaint();
 					JOptionPane.showMessageDialog(null, "Game Loaded.");
