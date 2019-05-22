6846c0f8509a3f93b3296dae0c0a266a9d8b15b2
Andrew He <43284720+AndyHuh98@users.noreply.github.com>
2019-05-21 21:46:05 -0500

    Update Dragon.java

--- The Dragon's Treasure/Dragon.java
+++ The Dragon's Treasure/Dragon.java
@@ -6,14 +6,19 @@ import javax.swing.JOptionPane;
 
 import java.util.Random;
 
+/* The dragon class is used for the antagonist of the game; the dragons. It extends the Sprite class,
+which allows it to take on an image and moves based on looped calls to it's image being updated constantly,
+along with its updated X and Y coordinate values. */
+
 public class Dragon extends Sprite {
-	private int maxHealth = 100;
-	private int currentHealth;
-	private int moveDirection;
-	private Projectile fireball;
-	private static Timer timer = new Timer();
-	private static boolean isNotDead = true;
+	private int maxHealth = 100; //default max Health
+	private int currentHealth; //current health (decreases each time dragon is struck by arrow)
+	private int moveDirection; 
+	private Projectile fireball; //No longer in use, fireball objects are just created in Model instead.
+	private static Timer timer = new Timer(); //No longer in use, previously was used to move the dragon.
+	private static boolean isNotDead = true; //Used in other methods to check if the dragon has 0 health or not.
 	
+	//Default constructor
 	public Dragon() {
 		super("Dragon.png");
 		super.setX(350);
@@ -21,8 +26,10 @@ public class Dragon extends Sprite {
 		currentHealth = 100;
 	}
 	
+	//Constructor, with a level parameter.
 	public Dragon(int level) {
 		super("Dragon" + level + ".png");
+		//Necessary because I only included 4 unique dragon sprites. If more are added, can increase the 3.
 		if (level > 3) {
 			setImage("Dragon3.png");
 		}
@@ -30,13 +37,17 @@ public class Dragon extends Sprite {
 		setDragonHealth(getDragonMaxHealth());
 	}
 	
+	//Checks if dragon is alive.
 	public void alive() {
 		isNotDead = true;
 	}
-
+	
+	//Used to set the move direction in Model
 	public void setMoveDirection(int x) {
 		moveDirection = x;
 	}
+	
+	//A blob of getters/setters.
 	public int getMoveDirection() { return moveDirection; }
 	public int getDragonMaxHealth() { return maxHealth; }
 	public void setMaxHealth(int max) { maxHealth = max; }
@@ -45,6 +56,9 @@ public class Dragon extends Sprite {
 		currentHealth = health; 
 	}
 	public boolean getDragonIsNotDead() { return isNotDead; }
+	
+	//Called in Model whenever the arrow hits the dragon. Gets the damage of the arrow and subtracts it
+	//from current health
 	public void arrowHit() {
 		if (isNotDead) {
 			if (getDragonCurrentHealth() - Arrow.getDamage() >= 0) {
@@ -71,6 +85,8 @@ public class Dragon extends Sprite {
 		}
 	}
 	
+	//The updatedImage method inherited from Sprite -- draws the image at the new X and Y location.
+	//Called in Model.
 	@Override
 	public synchronized void updateImage(Graphics g) {
 		g.drawImage(getImage(), getX(), getY(), 350, 350, null);
